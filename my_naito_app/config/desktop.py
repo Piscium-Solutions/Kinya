0258de8cf185fd160bb2c6da05090720682b0674
Piscium-Solutions <123194266+Piscium-Solutions@users.noreply.github.com>
2024-03-11 23:58:38 +0000

    Desktop

---
+++ my_naito_app/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "Banking",
+            "color": "#589494",
+            "icon": "octicon octicon-bank",
+            "type": "module",
+            "label": _("Banking")
+        }
+    ]
