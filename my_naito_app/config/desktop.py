bc9b4457d32931386fbea601b1a052f434d8ff76
Piscium-Solutions <123194266+Piscium-Solutions@users.noreply.github.com>
2024-03-12 19:20:06 +0000

    Module Setup

--- my_naito_app/config/desktop.py
+++ my_naito_app/config/desktop.py
@@ -3,10 +3,25 @@ from frappe import _
 def get_data():
     return [
         {
-            "module_name": "Banking",
-            "color": "#589494",
-            "icon": "octicon octicon-bank",
+            "module_name": "Retails",
+            "color": "#3498db",
+            "icon": "octicon octicon-repo",
             "type": "module",
-            "label": _("Banking")
+            "label": _("Retails")
+        },
+        {
+            "label": _("Banking Module"),
+            "icon": "octicon octicon-briefcase",
+            "items": [
+                {
+                    "type": "module",
+                    "name": "BankingModule",
+                    "label": _("Custom Banking Module"),
+                    "icon": "octicon octicon-briefcase",
+                    "color": "#3498db",
+                    "link": "#modules/BankingModule",
+                    "category": _("Modules"),
+                }
+            ]
         }
     ]
